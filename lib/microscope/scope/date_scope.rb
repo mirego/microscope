bb1fd17396aedfa52992fe0497a3be30d986c6f2
Rémi Prévost <remi@exomel.com>
2013-08-12 15:00:00 -0400

    Split complex methods into multiple ones

--- lib/microscope/scope/date_scope.rb
+++ lib/microscope/scope/date_scope.rb
@@ -2,21 +2,42 @@ module Microscope
   class Scope
     class DateScope < Scope
       def apply
-        cropped_field = field.name.gsub(/_on$/, '')
+        @cropped_field = field.name.gsub(/_on$/, '')
 
+        apply_before_scopes
+        apply_after_scopes
+        apply_between_scopes
+        apply_boolean_scopes
+      end
+
+    protected
+
+      def apply_before_scopes
         model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          scope "#{cropped_field}_before", lambda { |time| where("#{field.name} < ?", time) }
-          scope "#{cropped_field}_before_or_on", lambda { |time| where("#{field.name} <= ?", time) }
-          scope "#{cropped_field}_before_today", lambda { where("#{field.name} < ?", Date.today) }
+          scope "#{@cropped_field}_before", lambda { |time| where("#{field.name} < ?", time) }
+          scope "#{@cropped_field}_before_or_at", lambda { |time| where("#{field.name} <= ?", time) }
+          scope "#{@cropped_field}_before_today", lambda { where("#{field.name} < ?", Date.today) }
+        RUBY
+      end
 
-          scope "#{cropped_field}_after", lambda { |time| where("#{field.name} > ?", time) }
-          scope "#{cropped_field}_after_or_at", lambda { |time| where("#{field.name} >= ?", time) }
-          scope "#{cropped_field}_after_today", lambda { where("#{field.name} > ?", Date.today) }
+      def apply_after_scopes
+        model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          scope "#{@cropped_field}_after", lambda { |time| where("#{field.name} > ?", time) }
+          scope "#{@cropped_field}_after_or_at", lambda { |time| where("#{field.name} >= ?", time) }
+          scope "#{@cropped_field}_after_today", lambda { where("#{field.name} > ?", Date.today) }
+        RUBY
+      end
 
-          scope "#{cropped_field}_between", lambda { |range| where("#{field.name}" => range) }
+      def apply_between_scopes
+        model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          scope "#{@cropped_field}_between", lambda { |range| where("#{field.name}" => range) }
+        RUBY
+      end
 
-          scope "#{cropped_field}", lambda { where("#{field.name} IS NOT NULL AND #{field.name} <= ?", Date.today) }
-          scope "not_#{cropped_field}", lambda { where("#{field.name} IS NULL OR #{field.name} > ?", Date.today) }
+      def apply_boolean_scopes
+        model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          scope "#{@cropped_field}", lambda { where("#{field.name} IS NOT NULL AND #{field.name} <= ?", Date.today) }
+          scope "not_#{@cropped_field}", lambda { where("#{field.name} IS NULL OR #{field.name} > ?", Date.today) }
         RUBY
       end
     end
