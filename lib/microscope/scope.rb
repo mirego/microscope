20e1c3fa8693aeb11b695f78cbc8bd2e4abac695
Rémi Prévost <remi@exomel.com>
2013-08-23 14:09:06 -0400

    Store @field_name and @table_name

--- lib/microscope/scope.rb
+++ lib/microscope/scope.rb
@@ -1,5 +1,12 @@
 module Microscope
   class Scope < Struct.new(:model, :field)
+    def initialize(*args)
+      super
+
+      @field_name = field.name
+      @table_name = model.name.tableize
+    end
+
     # Inject ActiveRecord scopes into a model
     def self.inject_scopes(model, fields, options = {})
       fields.each do |field|
