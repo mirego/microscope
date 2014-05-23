6d0d7a0af6f0f4186fd6355d91d1369c6b8cce06
Rémi Prévost <remi@exomel.com>
2014-05-23 16:06:52 -0400

    Consolidate methods in DatetimeScope

--- lib/microscope/scope/datetime_scope.rb
+++ lib/microscope/scope/datetime_scope.rb
@@ -10,42 +10,23 @@ module Microscope
       end
 
       def apply
-        return unless @field_name =~ @cropped_field_regex
-
-        model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          #{before_scopes}
-          #{after_scopes}
-          #{between_scopes}
-          #{boolean_scopes}
-        RUBY
+        model.class_eval(apply_scopes) if @field_name =~ @cropped_field_regex
       end
 
     private
 
-      def before_scopes
+      def apply_scopes
         <<-RUBY
           scope "#{cropped_field}_before", lambda { |time| where('#{quoted_field} < ?', time) }
           scope "#{cropped_field}_before_or_at", lambda { |time| where('#{quoted_field} <= ?', time) }
           scope "#{cropped_field}_before#{@now_suffix}", lambda { where('#{quoted_field} < ?', #{@now}) }
-        RUBY
-      end
 
-      def after_scopes
-        <<-RUBY
           scope "#{cropped_field}_after", lambda { |time| where('#{quoted_field} > ?', time) }
           scope "#{cropped_field}_after_or_at", lambda { |time| where('#{quoted_field} >= ?', time) }
           scope "#{cropped_field}_after#{@now_suffix}", lambda { where('#{quoted_field} > ?', #{@now}) }
-        RUBY
-      end
 
-      def between_scopes
-        <<-RUBY
           scope "#{cropped_field}_between", lambda { |range| where("#{@field_name}" => range) }
-        RUBY
-      end
 
-      def boolean_scopes
-        <<-RUBY
           scope "#{cropped_field}", lambda { where('#{quoted_field} IS NOT NULL AND #{quoted_field} <= ?', #{@now}) }
           scope "not_#{cropped_field}", lambda { where('#{quoted_field} IS NULL OR #{quoted_field} > ?', #{@now}) }
           scope "un#{cropped_field}", lambda { not_#{cropped_field} }
